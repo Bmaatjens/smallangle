fa22a6b3c4763862c46d8a9d36c6d497d7477045
Bmaatjens <bmaatjens@gmail.com>
2023-11-16 11:35:16 +0100

    completed smallangle and smallangle doctrings
    with added comments

--- src/smallangle/smallangle.py
+++ src/smallangle/smallangle.py
@@ -4,19 +4,28 @@ from numpy import pi
 import pandas as pd
 import click
 
+# ensures that you can use multiple commands for different functions
+
 @click.group()
 def function():
     pass
 
+# name that needs to be called upon in terminal to run
+
 @function.command("sine")
+
+# the input value can be given after typing either one of the strings
+
 @click.option(
     '-n',
     '--number',
     default = 1
 )
 
+# a sine function working with an input value
+
 def sin(number):
-    """the number you put in, the sine function acts
+    """the number you put in, the sine function acts upon
 
     Args:
         number (float): any value except for infinity
@@ -29,12 +38,19 @@ def sin(number):
     print(df)
     return
 
+# name that needs to be called upon in terminal to run
+
 @function.command("tan")
+
+# the input value can be given after typing either one of the strings
+
 @click.option(  
     '-n',
     '--number',
     default = 1
 )
+
+# a tan function working with an input value
 
 def tan(number):
     """the number you put in, the tan function acts upon
@@ -50,6 +66,7 @@ def tan(number):
     print(df)
     return
 
+# if you call upon the name of either named function, this makes sure the function will run
 
 if __name__ == "__main__":
     function()
